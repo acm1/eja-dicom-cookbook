5460b59b42d224512fc7a9d3cd08c2b1b46fcf5c
Adam Mielke <adam@umn.edu>
2013-05-13 10:30:02 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# dicom cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Adam Mielke, (C) Regents of the University of Minnesota (<adam@umn.edu>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "dicom"
+maintainer       "Adam Mielke, (C) Regents of the University of Minnesota"
+maintainer_email "adam@umn.edu"
+license          "All rights reserved"
+description      "Installs/Configures dicom"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
